ffefd4836c9808f01feec023708c57769051908a
jrdonat <jrddonat@gmail.com>
2022-12-01 20:34:22 -0800

    Remove Excess Comments

--- AoC22/Day1/Part2.lua
+++ AoC22/Day1/Part2.lua
@@ -7,31 +7,24 @@ local testHighest = 0
 
 local t = {}
 
--- Loop through each line
 for line in file:lines() do
-    -- Check if line is blank
     if line == "" or line == "%s+" then
-        -- Check if testHighest is higher than highest
         if testHighest > highest then
-            -- Set highest to testHighest
             highest = testHighest
             table.insert(t, testHighest)
             testHighest = 0
         else
-            -- Set testHighest to 0
             table.insert(t, testHighest)
             testHighest = 0
         end
     else
-        -- Add to testHighest
         testHighest = testHighest + tonumber(line)
     end
 end
-if testHighest > highest then -- Line == "" won't fire so we have to run it one more time
+if testHighest > highest then
     highest = testHighest
 end
 
--- Sort table smallest to largest
 table.sort(t)
 local a,b,c = table.unpack(t,#t-2)
 print(a+b+c)
