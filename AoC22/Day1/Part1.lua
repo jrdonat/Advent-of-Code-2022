171c4c6986c492b5faacc2b1f1edbcc398a69208
jrdonat <101962091+jrdonat@users.noreply.github.com>
2022-12-01 20:26:08 -0800

    Add files via upload

---
+++ AoC22/Day1/Part1.lua
@@ -0,0 +1,31 @@
+-- AoC Day 1
+-- Clear output
+local file = io.open("D:\\AoC22\\Day1\\input.txt", "r")
+if not file then return end
+
+local highest = 0
+local testHighest = 0
+
+-- Loop through each line
+for line in file:lines() do
+    -- Check if line is blank
+    if line == "" or line == "%s+" then
+        -- Check if testHighest is higher than highest
+        if testHighest > highest then
+            -- Set highest to testHighest
+            highest = testHighest
+            testHighest = 0
+        else
+            -- Set testHighest to 0
+            testHighest = 0
+        end
+    else
+        -- Add to testHighest
+        testHighest = testHighest + tonumber(line)
+    end
+end
+if testHighest > highest then -- Line == "" won't fire so we have to run it one more time
+    highest = testHighest
+end
+
+print(highest)
